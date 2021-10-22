bddfd9e62c9050934600c3f05fcac0809a6ec32c
Sergey <gr00shik.aws0@gmail.com>
2021-10-22 21:22:52 +0300

    add log

--- untitled/src/Service.java
+++ untitled/src/Service.java
@@ -1,10 +1,12 @@
 public class Service {
 
     public void print() {
+        System.out.println("this is print method");
         System.out.println("Hello");
     }
 
     public int sum(int i1, int i2) {
+        System.out.println("try to sum two int values");
         return i1 + i2;
     }
 
